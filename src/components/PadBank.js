30831cee3a33f957bc556db5c24773a2abc1ab83
Yelan Tao <yeltao@student.ethz.ch>
2021-07-01 22:27:11 +0200

    simplify padbank

--- src/components/PadBank.js
+++ src/components/PadBank.js
@@ -3,32 +3,17 @@ import DrumPad from "./DrumPad";
 
 export default function PadBank({ currentPadBank, power, updateDisplay }) {
   let padBank;
-  if (power) {
-    padBank = currentPadBank.map((drumObj, i, padBankArr) => {
-      return (
-        <DrumPad
-          clip={padBankArr[i].url}
-          clipId={padBankArr[i].id}
-          keyCode={padBankArr[i].keyCode}
-          keyTrigger={padBankArr[i].keyTrigger}
-          power={power}
-          updateDisplay={updateDisplay}
-        />
-      );
-    });
-  } else {
-    padBank = currentPadBank.map((drumObj, i, padBankArr) => {
-      return (
-        <DrumPad
-          clip="#"
-          clipId={padBankArr[i].id}
-          keyCode={padBankArr[i].keyCode}
-          keyTrigger={padBankArr[i].keyTrigger}
-          power={power}
-          updateDisplay={updateDisplay}
-        />
-      );
-    });
-  }
+  padBank = currentPadBank.map((drumObj, i, padBankArr) => {
+    return (
+      <DrumPad
+        clip={power ? padBankArr[i].url : "#"}
+        clipId={padBankArr[i].id}
+        keyCode={padBankArr[i].keyCode}
+        keyTrigger={padBankArr[i].keyTrigger}
+        power={power}
+        updateDisplay={updateDisplay}
+      />
+    );
+  });
   return <div className="pad-bank">{padBank}</div>;
 }
